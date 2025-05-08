cf31db3aa5cb6f37e869e87d8a53899cf0916e9a
MUZAMMILATEO <an.ar54040@gmail.com>
2025-05-08 21:16:02 +0200

    initial commit

---
+++ settings.py
@@ -0,0 +1,5 @@
+# Common paths for the whole project are defined here.
+
+ASSETS_PATH = "/home/khanm2004/workfolder/ColonSLAM/assets/"
+DATA_PATH = "/home/khanm2004/workfolder/ColonSLAM/topometric/topometric/" 
+EVAL_PATH = "/home/jmorlana/Experiments/topometric_TMI_final/"
